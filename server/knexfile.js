5f8715952b11b09f09061361d6f022df87f77f26
James Freeman <freeman.james.h@gmail.com>
2016-05-20 14:30:57 -0600

    knexfile modded

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,13 +1,23 @@
+require('dotenv').config();
+
 module.exports = {
 
   development: {
-    client: 'pg',
-    connection: 'postgres://localhost/animals'
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min:2,
+      max:10
+    }
   },
 
-
   production: {
-        client: 'pg',
-        connection: process.env.DATABASE_URL + '?ssl=true'
-      }
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min:2,
+      max:10
+    }
+  },
+
 };
