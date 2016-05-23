f880663d47d547375dd11279e580ecab21ff428c
James Freeman <freeman.james.h@gmail.com>
2016-05-23 13:33:17 -0600

    hardcoded object in routes for testing

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,7 +6,7 @@ var knex = require('../db/knex.js');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   knex('users').select().then(function(results){
-    res.json(results)
+    res.json({james: 'yes'});
   })
 });
 
