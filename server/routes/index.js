b4c2b17ae5c03a225437776fcfbd42a3651ece5d
James Freeman <freeman.james.h@gmail.com>
2016-05-23 13:38:41 -0600

    hardcoding json object worked, now trying heroku database

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,7 +6,9 @@ var knex = require('../db/knex.js');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   knex('users').select().then(function(results){
-    res.json({james: 'yes'});
+    res.json('users').then(function(results){
+      return results
+    });
   })
 });
 
