22db364fa301cae87c4677f5a327eb4995da440f
James Freeman <freeman.james.h@gmail.com>
2016-05-23 13:40:23 -0600

    fixed typo

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,8 +6,7 @@ var knex = require('../db/knex.js');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   knex('users').select().then(function(results){
-    res.json('users').then(function(results){
-      return results
+    res.json(results)
     });
   })
 });
