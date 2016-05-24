a2be136e03078e8bdd0ab152dd84d21ee29d82bc
Matthew Bouchard <bouchard.matthewj@gmail.com>
2016-05-24 15:33:57 -0600

    commit before pull

--- server/api/api.js
+++ server/api/api.js
@@ -32,7 +32,6 @@ router.post('/newlocation', function(req, res, next) {
 //login w/token
 
 router.get('/me', function(req, res, next) {
-
   if (req.headers.authorization) {
     const token = req.headers.authorization.split(' ')[1];
     const payload = jwt.verify(token, process.env.JWT_SECRET);
