07be1bbbf9fcd72861cb60a8fc670e51816f8a77
James Freeman <freeman.james.h@gmail.com>
2016-05-24 13:49:32 -0600

    added cors

--- server/app.js
+++ server/app.js
@@ -4,11 +4,12 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 var api = require('./api/api');
 
 var app = express();
-
+app.use(cors());
 
 
 // uncomment after placing your favicon in /public
