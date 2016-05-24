0957d8cbcd1d636bdf85d1bae0d27af0fe43b0a4
James Freeman <freeman.james.h@gmail.com>
2016-05-24 11:01:33 -0600

    signup w/ bcrypt implemented

--- server/api/api.js
+++ server/api/api.js
@@ -22,10 +22,43 @@ router.post('/newlocation', function(req, res, next) {
   })
 });
 
+//signup w/ bcrypt
+
+router.post('/signup', function(req, res, next) {
+  console.log('postrequest received')
+  knex('users')
+    .whereRaw('lower(username) = ?', req.body.username.toLowerCase())
+    .count()
+    .first()
+    .then(function(result) {
+      if (result.count === "0") {
+        var saltRounds = 4;
+        var passwordHash = bcrypt.hashSync(req.body.password, saltRounds)
+        console.log('inserting')
+        knex('users').insert({
+            username: req.body.username,
+            password: passwordHash
+          })
+          .returning('*').then(function(userReturn) {
+            var user = (userReturn[0])
+            // var token = jwt.sign({
+            //   id: user.id
+            // }, 'secret')
+
+            res.json({
+              id: user.id,
+              email: user.email,
+              name: user.name,
+              // token: token
+            })
+          })
+      } else {
+        res.status(422).json({
+          errors: ['Email already taken']
+        })
+      }
+    })
+})
+
+
 module.exports = router;
-
-//signup
-
-// router.post('/', function(req, res, next){
-//   knex('users').where('name')
-// })
