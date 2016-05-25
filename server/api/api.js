00b680c3eac1d08abc8d5daaee82b163360845be
James Freeman <freeman.james.h@gmail.com>
2016-05-25 16:57:20 -0600

    minimum functionality in /goactive route for testing

--- server/api/api.js
+++ server/api/api.js
@@ -7,7 +7,7 @@ var jwt = require('jsonwebtoken');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  knex('users').select().then(function(results){
+  knex('users').select().then(function(results) {
     res.json(results)
   })
 });
@@ -17,9 +17,14 @@ router.get('/me', function(req, res, next) {
     const token = req.headers.authorization.split(' ')[1];
     const payload = jwt.verify(token, process.env.JWT_SECRET);
     // payload is {id: 56}
-    knex('users').where({id: payload.id}).first().then(function (user) {
+    knex('users').where({
+      id: payload.id
+    }).first().then(function(user) {
       if (user) {
-        res.json({id: user.id, name: user.username})
+        res.json({
+          id: user.id,
+          name: user.username
+        })
       } else {
         res.status(403).json({
           error: "Invalid ID"
@@ -35,85 +40,104 @@ router.get('/me', function(req, res, next) {
 
 
 //'filter and miles'
-router.get('/buddylist', function(req, res, next){
-  var userLat = 40.0179;
-  var userLong = -105.28;
-  knex.raw('SELECT * FROM users WHERE acos(sin(' + userLat + ' * PI() / 180) * sin(lat * PI() / 180) + cos(' + userLat +' * PI() / 180) * cos(lat * PI() / 180) * cos((long * PI() / 180) - ('+ userLong +' * PI() / 180))) * 3959 <= 5 AND active = true' )
-  .then(function(results){
-    res.json(results.rows)
-  })
+router.post('/goactive', function(req, res, next) {
+  var userID = req.body.id
+  var userLat = req.body.lat;
+  var userLong = req.body.long;
+  var activeBool = req.body.active;
+  res.json({connection: 'established :) :) :) ;) :)'})
+
+  // knex('users').where('id', userID).update({
+  //   lat: userLat,
+  //   long: userLong,
+  //   active: activeBool
+  // })
+
+
+  // if (activeBool === true) {
+  //   knex.raw('SELECT * FROM users WHERE acos(sin(' + userLat + ' * PI() / 180) * sin(lat * PI() / 180) + cos(' + userLat + ' * PI() / 180) * cos(lat * PI() / 180) * cos((long * PI() / 180) - (' + userLong + ' * PI() / 180))) * 3959 <= 5 AND active = true')
+  //     .then(function(results) {
+  //       res.json(results.rows)
+  //     })
+  // } else {
+  //   res.json({message: 'buddy list should be empty now'})
+  // }
 })
 
 
-router.post('/newlocation', function(req, res, next) {
-  knex('users')
-  .where('username', '=', req.body.username.toLowerCase())
-  .first()
-  .update(req.body)
-  .returning('*')
-  .then(function(response){
-    console.log('response from server location route: ', response)
-    res.json(response)
-  });
-});
+// router.post('/newlocation', function(req, res, next) {
+//   knex('users')
+//   .where('username', '=', req.body.username.toLowerCase())
+//   .first()
+//   .update(req.body)
+//   .returning('*')
+//   .then(function(response){
+//     console.log('response from server location route: ', response)
+//     res.json(response)
+//   });
+// });
 //signup w/ bcrypt
+
+
 router.post('/signup', function(req, res, next) {
-  console.log('postrequest received')
-  knex('users')
-  .whereRaw('lower(username) = ?', req.body.username.toLowerCase())
-  .count()
-  .first()
-  .then(function(result) {
-    if (result.count === "0") {
-      var saltRounds = 4;
-      var passwordHash = bcrypt.hashSync(req.body.password, saltRounds)
-      console.log('inserting')
-      knex('users').insert({
-        username: req.body.username,
-        password: passwordHash
+    console.log('postrequest received')
+    knex('users')
+      .whereRaw('lower(username) = ?', req.body.username.toLowerCase())
+      .count()
+      .first()
+      .then(function(result) {
+        if (result.count === "0") {
+          var saltRounds = 4;
+          var passwordHash = bcrypt.hashSync(req.body.password, saltRounds)
+          console.log('inserting')
+          knex('users').insert({
+              username: req.body.username,
+              password: passwordHash
+            })
+            .returning('*').then(function(userReturn) {
+              var user = (userReturn[0])
+              var token = jwt.sign({
+                id: user.id
+              }, process.env.JWT_SECRET)
+              res.json({
+                id: user.id,
+                name: user.username,
+                token: token
+              })
+            })
+        } else {
+          res.status(422).json({
+            errors: ['Email already taken']
+          })
+        }
       })
-      .returning('*').then(function(userReturn) {
-        var user = (userReturn[0])
-        var token = jwt.sign({
+  })
+  //login w/ bcrypt
+router.post('/login', function(req, res, next) {
+  knex('users')
+    .where('username', '=', req.body.username.toLowerCase())
+    .first()
+    .then(function(response) {
+      // error check for email??
+      if (response && bcrypt.compareSync(req.body.password, response.password)) {
+        console.log('user found');
+        //  console.log('from the response promise:', response)
+        const user = response;
+        console.log('user: ', user)
+        const token = jwt.sign({
           id: user.id
-        }, process.env.JWT_SECRET)
+        }, process.env.JWT_SECRET);
+        console.log('token', token)
         res.json({
           id: user.id,
-          name: user.username,
+          // email: user.email,
+          username: user.username,
           token: token
         })
-      })
-    } else {
-      res.status(422).json({
-        errors: ['Email already taken']
-      })
-    }
-  })
-})
-//login w/ bcrypt
-router.post('/login', function(req,res,next) {
-  knex('users')
-  .where('username', '=', req.body.username.toLowerCase())
-  .first()
-  .then(function(response){
-    // error check for email??
-    if(response && bcrypt.compareSync(req.body.password, response.password)){
-      console.log('user found');
-      //  console.log('from the response promise:', response)
-      const user = response;
-      console.log('user: ',user)
-      const token = jwt.sign( {id:user.id} , process.env.JWT_SECRET);
-      console.log('token',token)
-      res.json({
-        id: user.id,
-        // email: user.email,
-        username: user.username,
-        token: token
-      })
-    } else {
-      res.status(422).send('Invalid username or password')
-    }
-  });
+      } else {
+        res.status(422).send('Invalid username or password')
+      }
+    });
 })
 
 
