f58a5cd58317c386d78a56c2692d0c9434054235
James Freeman <freeman.james.h@gmail.com>
2016-05-26 14:51:38 -0600

    still fiddling with goactive

--- server/api/api.js
+++ server/api/api.js
@@ -45,7 +45,7 @@ router.post('/goactive', function(req, res, next) {
   var userLat = req.body.lat;
   var userLong = req.body.long;
   var activeBool = req.body.active;
-  knex('users').where('id', 21).update({
+  knex('users').where('id', '21').update({
     lat: userLat,
     long: userLong,
     active: true
@@ -54,7 +54,7 @@ router.post('/goactive', function(req, res, next) {
   if (activeBool === true) {
     knex.raw('SELECT * FROM users WHERE acos(sin(' + userLat + ' * PI() / 180) * sin(lat * PI() / 180) + cos(' + userLat + ' * PI() / 180) * cos(lat * PI() / 180) * cos((long * PI() / 180) - (' + userLong + ' * PI() / 180))) * 3959 <= 5 AND active = true')
       .then(function(results) {
-        res.json(results.rows)
+        res.json(req.body)
       })
   } else {
     res.json({message: 'buddy list should be empty now'})
