1cac201d7201d3c4ca36e86ea334c1e7885e99b7
James Freeman <freeman.james.h@gmail.com>
2016-05-26 16:01:54 -0600

    testing continues. checking to see if update returns promise

--- server/api/api.js
+++ server/api/api.js
@@ -52,7 +52,9 @@ router.post('/goactive', function(req, res, next) {
     lat: userLat,
     long: userLong,
     active: activeBool
-  });
+  }).then(function(){
+    console.log('knex insertion complete')
+  })
 
   if (activeBool === true) {
     knex.raw('SELECT * FROM users WHERE acos(sin(' + userLat + ' * PI() / 180) * sin(lat * PI() / 180) + cos(' + userLat + ' * PI() / 180) * cos(lat * PI() / 180) * cos((long * PI() / 180) - (' + userLong + ' * PI() / 180))) * 3959 <= 5 AND active = true')
